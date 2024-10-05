9773f9d4810a9178868a91192aaac997dceb443f
Davide Cioni <88600810+deivshon@users.noreply.github.com>
2024-10-05 23:26:04 +0200

    feat: ✨ add todo project structure/api layer sections in thesis

--- elm/todo/fe/src/Api.elm
+++ elm/todo/fe/src/Api.elm
@@ -4,21 +4,36 @@ import Http
 import Todo
 
 
-getTodos : (Result Http.Error (List Todo.Todo) -> msg) -> Cmd msg
+baseUrl : String
+baseUrl =
+    "http://localhost:3000"
+
+
+getTodos :
+    (Result Http.Error (List Todo.Todo)
+     -> msg
+    )
+    -> Cmd msg
 getTodos toMsg =
     Http.get
-        { url = "http://localhost:3000/todos"
+        { url = baseUrl ++ "/todos"
         , expect = Http.expectJson toMsg Todo.todoListDecoder
         }
 
 
-updateTodo : String -> Todo.TodoUpdateBody -> (Result Http.Error () -> msg) -> Cmd msg
-updateTodo id todoDoneUpdate toMsg =
+updateTodo :
+    String
+    -> Todo.TodoUpdateBody
+    -> (Result Http.Error () -> msg)
+    -> Cmd msg
+updateTodo id todoUpdate toMsg =
     Http.request
         { method = "PATCH"
         , headers = []
-        , url = "http://localhost:3000/todos/" ++ id
-        , body = Http.jsonBody (Todo.todoUpdateEncoder todoDoneUpdate)
+        , url = baseUrl ++ "/todos/" ++ id
+        , body =
+            Http.jsonBody
+                (Todo.todoUpdateEncoder todoUpdate)
         , expect = Http.expectWhatever toMsg
         , timeout = Nothing
         , tracker = Nothing
@@ -28,7 +43,7 @@ updateTodo id todoDoneUpdate toMsg =
 createTodo : Todo.NewTodo -> (Result Http.Error Todo.Todo -> msg) -> Cmd msg
 createTodo content toMsg =
     Http.post
-        { url = "http://localhost:3000/todos"
+        { url = baseUrl ++ "/todos"
         , body = Http.jsonBody (Todo.newTodoEncoder content)
         , expect = Http.expectJson toMsg Todo.todoDecoder
         }
@@ -39,7 +54,7 @@ deleteTodo id toMsg =
     Http.request
         { method = "DELETE"
         , headers = []
-        , url = "http://localhost:3000/todos/" ++ id
+        , url = baseUrl ++ "/todos/" ++ id
         , body = Http.emptyBody
         , expect = Http.expectWhatever toMsg
         , timeout = Nothing
